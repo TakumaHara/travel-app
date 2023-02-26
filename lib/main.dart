713df2a7ab70d5a61a0aeba88e7071e0c5d0d120
Takuma <hara.takuma@gmail.com>
2023-02-26 20:37:47 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String infoText = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              controller: emailController,
+            ),
+            TextFormField(
+              controller: passwordController,
+            ),
+            Text(infoText),
+            ElevatedButton(onPressed: () async {
+
+              try{
+                final FirebaseAuth auth = FirebaseAuth.instance;
+                final UserCredential user = await auth.createUserWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text
+                );
+              }catch(e){
+                setState(() {
+                  infoText = "ログインに失敗しました";
+                });
+              }
+            }, child: Text("登録"))
+          ],
+        ),
+      ),
+    );
+  }
+}
